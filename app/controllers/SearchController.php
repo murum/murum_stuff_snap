5002dc2490a28e28535c9018b02e970c650b064c
John Svensson <john.svensson@web2life.se>
2014-07-27 00:22:39 +0200

    Revert "Fioxes"
    This reverts commit e7552366234614c495bd795d7b6b222efc6dfe89.

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -8,23 +8,22 @@ class SearchController extends BaseController {
 
     public function search() {
 
-        $query = User::query();
+        $users = DB::table('users');
 
         if (Input::has('minAge') && Input::has('maxAge')) {
-            $query->whereBetween('age', array(Input::get('minAge'), Input::get('maxAge')));
+            $users = $users->whereBetween('age', array(Input::get('minAge'), Input::get('maxAge')));
         } else if (Input::has('minAge')) {
-            $query->where('age', '>=', Input::get('minAge'));
+            $users = $users->where('age', '>=', Input::get('minAge'));
         } else if (Input::has('maxAge')) {
-            $query->where('age', '>=', Input::get('maxAge'));
+            $users = $users->where('age', '>=', Input::get('maxAge'));
         }
 
         if (Input::has('sex') && Input::get('sex') !== "0") {
-            $query->where('sex', '=', Input::get('sex'));
+            $users = $users->where('sex', '=', Input::get('sex'));
         }
 
-        $users = $query->paginate(15);
+        $users = $users->paginate(15);
 
         return View::make('search.result', compact('users'));
-
     }
 }
