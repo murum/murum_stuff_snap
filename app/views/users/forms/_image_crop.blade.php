f7c3e4309952f44e3c9011439a2854e99c12e0f9
murum <cr222cs@student.lnu.se>
2014-07-26 23:36:12 +0200

    big frontend update of the creation of new cards

--- app/views/users/forms/_image_crop.blade.php
+++ app/views/users/forms/_image_crop.blade.php
@@ -1,36 +1,67 @@
 <div class="image-cropper">
-    <div class="col-xs-6">
-        <img id="image-cropper" src="" alt=""/>
-    </div>
-    <div class="col-xs-6 col-sm-4 col-md-3">
-        <div class="users-item users-item-{{ rand(1,4) }}">
-            <div class="users-item-image">
-                <img id="image-preview" src="" alt=""/>
+    <div class="row">
+        <div class="image-cropper-container">
+            <div class="col-xs-12 col-sm-offset-1 col-sm-6 col-md-7">
+                <div class="img-container">
+                    <img id="image-cropper" src="" alt=""/>
+                </div>
             </div>
-            <div class="users-item-name">
-                Examplename
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-3">
+            <div class="users-item users-item-{{ rand(1,4) }}">
+                <div class="users-item-image">
+                    <img id="image-preview" src="" alt=""/>
+                </div>
+                <div class="users-item-name">
+                    Examplename
+                </div>
+                <div class="users-item-gender female }}">
+                </div>
+                <hr class="users-item-divider" />
+                <div class="users-item-age">
+                    Age: XX
+                </div>
+                <div class="users-item-description">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi beatae corporis cupiditate.
+                    </p>
+                </div>
             </div>
-            <div class="users-item-gender female }}">
-            </div>
-            <hr class="users-item-divider" />
-            <div class="users-item-age">
-                Age: XX
-            </div>
-            <div class="users-item-description">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi beatae corporis cupiditate.
-                </p>
+            <div class="cropper-buttons-first">
+                <a class="btn btn-lg btn-block btn-success skip-crop">
+                    This looks awesome
+                </a>
+                <a class="btn btn-lg btn-block btn-primary show-crop">
+                    I need to modify this
+                </a>
             </div>
         </div>
     </div>
-    <div class="col-xs-12 col-sm-2 col-md-3">
-        {{ Form::open(['route' => ['images.update'], 'class' => 'form form-horizontal image-form-update']) }}
-            {{ Form::hidden('image-url') }}
-            {{ Form::hidden('x') }}
-            {{ Form::hidden('y') }}
-            {{ Form::hidden('w') }}
-            {{ Form::hidden('h') }}
-            {{ Form::submit('Update image', ['class' => 'btn btn-lg btn-success']) }}
-        {{ Form::close() }}
+    <div class="row hidden cropper-buttons">
+        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
+            <a id="skip-crop" class="btn btn-lg btn-block btn-danger">
+                Skip crop
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
+            <a id="reupload-image" class="btn btn-lg btn-block btn-warning">
+                <i class="fa fa-rotate-left"></i>
+                Reset
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 col-md-3">
+            {{ Form::open(['route' => ['images.update'], 'class' => 'form form-horizontal image-form-update']) }}
+                {{ Form::hidden('image-url') }}
+                {{ Form::hidden('image-width') }}
+                {{ Form::hidden('image-height') }}
+                {{ Form::hidden('x') }}
+                {{ Form::hidden('y') }}
+                {{ Form::hidden('w') }}
+                {{ Form::hidden('h') }}
+                <button type="submit" class="btn btn-lg btn-block btn-success">
+                    I'm done cropping
+                </button>
+            {{ Form::close() }}
+        </div>
     </div>
 </div>
