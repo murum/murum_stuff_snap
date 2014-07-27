daec7239fcc38a20ca2f8f75784dc087ded0d78b
John Svensson <john.svensson@web2life.se>
2014-07-27 15:51:35 +0200

    Add some rules

--- app/views/static/rules.blade.php
+++ app/views/static/rules.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
 <h1>Rules to use let's snap</h1>
+
+<ol>
+    <li>
+        Any <strong>username</strong> that contains any form of racism, sexual or associated with anything inappropriate such as drugs will be blocked and deleted.
+    </li>
+    <li>
+        Profile Picture that is inappropriate e.g. naked, stripped or daring will be deleted.
+    </li>
+    <li>
+        Do not post personal information about yourself such as address and lastname. We do allow firstname, city or state.
+    </li>
+    <li>
+        Advertising is not allowed and will be deleted.
+    </li>
+</ol>
+
 @stop
