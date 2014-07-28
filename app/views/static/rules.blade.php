1426fb10ca748cbc0fa817b9180d38d9a2145b1e
murum <cr222cs@student.lnu.se>
2014-07-28 12:27:24 +0200

    added rules

--- app/views/static/rules.blade.php
+++ app/views/static/rules.blade.php
@@ -1,5 +1,31 @@
 @extends('layout.master')
 
 @section('content')
-<h1>Rules to use let's snap</h1>
+<h1>Rules to use Let'ssnap</h1>
+<ul class="list-group">
+    <li class="list-group-item">
+        Any username that contains any form of racism, sexual or associated with anything inappropriate such as drugs will be blocked and deleted.
+    </li>
+    <li class="list-group-item">
+        We don't really have a age restriction just yet, but we recommend that you're above 12 years old to post on this page.
+    </li>
+    <li class="list-group-item">
+        Pictures that are inappropriate as in naked, undressed or dare will be deleted
+    </li>
+    <li class="list-group-item">
+        Advertising on posts / cards is completely prohibited
+    </li>
+    <li class="list-group-item">
+        Hacking or trying to hack is not allowed.
+    </li>
+    <li class="list-group-item">
+        Requests about naked picture is prohibited
+    </li>
+    <li class="list-group-item">
+        Please keep your privacy (such as address, name) private.
+    </li>
+    <li class="list-group-item">
+        The team of Let'ssnap do not take any responsibility whatsoever that users put up. We delete in after rules as time allows
+    </li>
+</ul>
 @stop
