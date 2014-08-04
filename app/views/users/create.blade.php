9c23a75be1912ed5d8a171c7ee99731971763000
murum <cr222cs@student.lnu.se>
2014-08-04 20:33:30 +0200

    removed old UX to create cards

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -4,8 +4,8 @@
     <h1>Add a card to the wall</h1>
     <div class="row">
         <div class="col-xs-12 col-sm-8">
-            @include('users.forms._user_form_new')
-            @include('users.forms._image_crop_new')
+            @include('users.forms._user_form')
+            @include('users.forms._image_crop')
         </div>
         <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
             <div class="users-item users-item-{{ rand(1,4) }}">
@@ -27,25 +27,4 @@
             </div>
         </div>
     </div>
-<!--
-<div class="row step-image">
-    <div class="col-xs-12">
-        <h2>Choose image</h2>
-        @include('users.forms._image_form')
-    </div>
-</div>
-
-<div class="row step-crop hidden">
-    <div class="col-xs-12">
-        @include('users.forms._image_crop')
-    </div>
-</div>
-
-<div class="row step-fields hidden">
-    <div class="col-xs-12">
-        <h2>Fill your information</h2>
-        @include('users.forms._user_form')
-    </div>
-</div>
--->
 @stop
--- app/views/users/forms/_image_crop.blade.php
+++ app/views/users/forms/_image_crop.blade.php
@@ -1,64 +1,37 @@
 <div class="image-cropper hidden">
     <div class="row">
         <div class="image-cropper-container">
-            <div class="col-xs-12 col-sm-offset-1 col-sm-6 col-md-7">
+            <div class="col-xs-12">
                 <div class="img-container">
                     <img id="image-cropper" src="" alt=""/>
                 </div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-4 col-md-3">
-            <div class="users-item users-item-{{ rand(1,4) }}">
-                <div class="users-item-image">
-                    <img id="image-preview" src="" alt=""/>
-                </div>
-                <div class="users-item-name">
-                </div>
-                <div class="users-item-gender female }}">
-                </div>
-                <hr class="users-item-divider" />
-                <div class="users-item-age">
-                    Age:
-                </div>
-                <div class="users-item-description">
-                    <p>
-                    </p>
-                </div>
-            </div>
-            <div class="cropper-buttons-first">
-                <a class="btn btn-lg btn-block btn-success skip-crop">
-                    This looks awesome
-                </a>
-                <a class="btn btn-lg btn-block btn-primary show-crop">
-                    I need to modify this
-                </a>
-            </div>
-        </div>
     </div>
-    <div class="row hidden cropper-buttons">
-        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
-            <a class="btn btn-lg btn-block btn-danger skip-crop">
+    <div class="row cropper-buttons">
+        <div class="col-xs-12 col-sm-4">
+            <a class="btn btn-block btn-danger skip-crop">
                 Skip crop
             </a>
         </div>
-        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
-            <a id="reupload-image" class="btn btn-lg btn-block btn-warning">
+        <div class="col-xs-12 col-sm-4">
+            <a id="reupload-image" class="btn btn-block btn-warning">
                 <i class="fa fa-rotate-left"></i>
                 Reset
             </a>
         </div>
-        <div class="col-xs-12 col-sm-4 col-md-3">
+        <div class="col-xs-12 col-sm-4">
             {{ Form::open(['route' => ['images.update'], 'class' => 'form form-horizontal image-form-update']) }}
-                {{ Form::hidden('image-url') }}
-                {{ Form::hidden('image-width') }}
-                {{ Form::hidden('image-height') }}
-                {{ Form::hidden('x') }}
-                {{ Form::hidden('y') }}
-                {{ Form::hidden('w') }}
-                {{ Form::hidden('h') }}
-                <button type="submit" class="btn btn-lg btn-block btn-success">
-                    I'm done cropping
-                </button>
+            {{ Form::hidden('image-url') }}
+            {{ Form::hidden('image-width') }}
+            {{ Form::hidden('image-height') }}
+            {{ Form::hidden('x') }}
+            {{ Form::hidden('y') }}
+            {{ Form::hidden('w') }}
+            {{ Form::hidden('h') }}
+            <button type="submit" class="btn btn-block btn-success">
+                I'm done cropping
+            </button>
             {{ Form::close() }}
         </div>
     </div>
