13dc464c98ca9cc5612f2b02ca38b125dfc2b798
murum <cr222cs@student.lnu.se>
2014-08-08 19:27:20 +0200

    fix search for mobile

--- app/views/search/forms/_search.blade.php
+++ app/views/search/forms/_search.blade.php
@@ -1,9 +1,9 @@
 {{ Form::open(['route' => ['search.post'], 'class' => 'form form-inline']) }}
 <div class="row">
-    <div class="col-xs-4">
+    <div class="col-xs-12 col-sm-4">
         {{ Form::select('sex', ['0' => Lang::get('letssnap.gender.both'), '1' => Lang::get('letssnap.gender.male'), '2' => Lang::get('letssnap.gender.female')], null, ['class' => 'form-control selectpicker']) }}
     </div>
-    <div class="col-xs-6">
+    <div class="col-xs-12 col-sm-6">
         <span class="text-info-light">
             {{ Lang::get('letssnap.search.age') }}
         </span>
@@ -11,7 +11,7 @@
         -
         {{ Form::text('maxAge', null, ['maxlength' => '3', 'class' => 'form-control']) }}
     </div>
-    <div class="col-xs-2 text-right">
+    <div class="col-xs-12 col-sm-2 text-right">
         <button type="submit" class="btn btn-success btn-search">
             <i class="fa fa-search"></i>
         </button>
