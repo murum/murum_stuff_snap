f2f6ddc3bf92d653ab1ebf0d2b446171797a0dd3
murum <cr222cs@student.lnu.se>
2014-08-16 00:17:59 +0200

    fix filter

--- app/views/search/forms/_search.blade.php
+++ app/views/search/forms/_search.blade.php
@@ -1,10 +1,10 @@
 {{ Form::open(['route' => ['search.post'], 'class' => 'form form-inline']) }}
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-3">
+    <div class="col-xs-12 col-sm-12 col-md-3 search-form-gender">
         {{ Form::select('sex', ['0' => Lang::get('letssnap.gender.both'), '1' => Lang::get('letssnap.gender.male'), '2' => Lang::get('letssnap.gender.female')], null, ['class' => 'form-control selectpicker']) }}
     </div>
 
-    <div class="col-xs-12 col-sm-12 col-md-7">
+    <div class="col-xs-12 col-sm-12 col-md-7 search-form-age">
         <span class="text-info-light">
             {{ Lang::get('letssnap.search.age') }}
         </span>
@@ -13,7 +13,7 @@
         {{ Form::text('maxAge', null, ['maxlength' => '3', 'class' => 'form-control']) }}
     </div>
 
-    <div class="hidden-xs hidden-sm col-md-2 text-right">
+    <div class="hidden-xs hidden-sm col-md-1 text-right">
         <button type="submit" class="btn btn-success btn-search">
             <i class="fa fa-search"></i>
         </button>
