e3448130030a1e4c9c578d7919f83cb8f75f38a8
murum <cr222cs@student.lnu.se>
2014-08-16 20:01:29 +0200

    update addthis

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -41,17 +41,21 @@
 {{ HTML::script('/js/libs.js') }}
 {{ HTML::script('/js/main.js') }}
 
-<!-- Google Analytics -->
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+@if ( URL::to('/') == 'http://letssnap.se' || URL::to('/') == 'http://www.letssnap.se' )
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-53331685-1', 'auto');
-    ga('send', 'pageview');
+        ga('create', 'UA-53331685-1', 'auto');
+        ga('send', 'pageview');
+    </script>
+@endif
 
-</script>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-53ef7f597dbaf532"></script>
 
 </body>
 </html>
