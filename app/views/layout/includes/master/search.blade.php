739581b29bdd0215f65fa74f1fad85f1c001c513
murum <cr222cs@student.lnu.se>
2014-08-24 17:04:16 +0200

    update search form for landscape iPad

--- app/views/layout/includes/master/search.blade.php
+++ app/views/layout/includes/master/search.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-offset-1 col-lg-10">
         <h3 class="no-margin-top">
             {{ Lang::get('letssnap.headers.filter_cards') }}
         </h3>
