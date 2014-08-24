bb67b8983a6e933c2419145ae417a54101dae343
murum <cr222cs@student.lnu.se>
2014-08-24 20:08:06 +0200

    update analytics code

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -56,7 +56,7 @@
 {{ HTML::script('/js/libs.js') }}
 {{ HTML::script('/js/main.js?v=2') }}
 
-@if ( URL::to('/') == 'http://letssnap.se' || URL::to('/') == 'http://www.letssnap.se' || URL::to('/') == 'http://www.letssnap.com' || URL::to('/') == 'http://letssnap.com' )
+@if ( URL::to('/') == 'http://letssnap.se' || URL::to('/') == 'http://www.letssnap.se')
     <!-- Google Analytics -->
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -66,6 +66,18 @@
 
         ga('create', 'UA-53331685-1', 'auto');
         ga('send', 'pageview');
+    </script>
+@elseif(URL::to('/') == 'http://www.letssnap.com' || URL::to('/') == 'http://letssnap.com')
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-53331685-2', 'auto');
+      ga('send', 'pageview');
+
     </script>
 @endif
 
