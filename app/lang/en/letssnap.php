f13b757ac7e4f6222ba28e042faa8883fb2c2be4
murum <cr222cs@student.lnu.se>
2014-08-25 18:44:53 +0200

    update english notice text

--- app/lang/en/letssnap.php
+++ app/lang/en/letssnap.php
@@ -91,7 +91,7 @@ return array(
 	'update' => array(
 		'news_one_paragraph_one' => 'Let\'s Snap have just been having a big facelift, We\'ve remade the card layout to make it even easier to get in touch with the users with direct links to their profiles. We\'ve also remade the way of how you create cards, now we\'re using the KIK profile picture instead of an own creted image uploader with cropping.',
 		'news_one_paragraph_two' => 'What\'s your thoughts on this remake? Please get in touch through contact@letssnap.com',
-		'notice' => 'Big update of Let\s snap, read more...',
+		'notice' => 'Big update of Let\'s snap, read more...',
 	),
     'about' => array(
         'paragraph_one' => "Let's snap is a new website to find and get in touch with new Snapchat friends.",
