e9da348fa0959dbe722aa7e8e39b9db8678d3433
murum <cr222cs@student.lnu.se>
2014-08-28 22:12:16 +0200

    update ads

--- app/views/partials/_top_ad.blade.php
+++ app/views/partials/_top_ad.blade.php
@@ -2,15 +2,27 @@
     <div class="row row-margin">
         <div class="col-xs-12">
             <div class="text-center">
-                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <!-- Let's snap top -->
-                <ins class="adsbygoogle"
-                     style="display:inline-block;width:728px;height:90px"
-                     data-ad-client="ca-pub-6725713569575731"
-                     data-ad-slot="7467491805"></ins>
-                <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                @if($app->getLocale() == 'sv')
+                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- Let's snap top -->
+                  <ins class="adsbygoogle"
+                       style="display:inline-block;width:728px;height:90px"
+                       data-ad-client="ca-pub-6725713569575731"
+                       data-ad-slot="7467491805"></ins>
+                  <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
+                @else
+                  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                  <!-- letssnap.com top -->
+                  <ins class="adsbygoogle"
+                       style="display:inline-block;width:728px;height:90px"
+                       data-ad-client="ca-pub-6725713569575731"
+                       data-ad-slot="7807935400"></ins>
+                  <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
+                @endif
             </div>
         </div>
     </div>
